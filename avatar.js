c1bbf99d27813c10d549ed2090aca23977eea72c
JL Swaine <jlswaine@me.com>
2022-11-10 12:03:35 -0800

    wip

---
+++ avatar.js
@@ -0,0 +1,8 @@
+const Avatar = () => {
+  return (<div>Totally React</div>);
+}
+
+
+const domContainer = document.querySelector('#react-container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(Avatar));
